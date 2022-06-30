2f65e8a2417923df6bb02bd06ec3393810dbcd09
Erich Pfaffenbach <49520647+Pfaffenbach@users.noreply.github.com>
2022-06-30 15:50:42 -0300

    Add
    pasta controle

---
+++ exercicios-js/controle/doWhile.js
@@ -0,0 +1,13 @@
+function getInteiroAleatorioEntre(min, max){
+    const valor = Math.random()*(max+min)
+    return Math.floor(valor)
+}
+
+let opcao //somente iniciada a variável, sem valor inicial!
+
+do{
+    opcao = getInteiroAleatorioEntre(-1,10)
+    console.log(`Opção encontrada foi ${opcao}.`)
+} while(opcao!=1)
+
+console.log('até a próxima!')
